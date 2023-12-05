0934284c9f549b10b433aaddb0901bb36a5eb742
Vanni Boncinelli <vanni.boncinelli@remira.com>
2023-12-05 11:33:48 +0100

    game completed!

--- Hangman/ConsoleApp1/Program.cs
+++ Hangman/ConsoleApp1/Program.cs
@@ -29,8 +29,72 @@ namespace Hangman
             // As long as the player has lives left, the game continues
             while (lives != 0)
             {
+                // counter of characters left to guess
+                int charactersLeft = 0;
 
+                // loop through all the characters of the chosen word
+                foreach (char character in wordToGuess)
+                {
+                    var letter = character.ToString();
+
+                    if (letters.Contains(letter))
+                    {
+                        Console.Write(letter);
+                    }
+                    else
+                    {
+                        Console.Write("_");
+
+                        charactersLeft++;
+                    }
+                }
+                Console.WriteLine(String.Empty);
+
+                if (charactersLeft == 0)
+                {
+                    break;
+                }
+                Console.Write("Type in a letter: ");
+
+                var key = Console.ReadKey().Key.ToString().ToLower();
+
+                Console.WriteLine(String.Empty);
+
+                if (!validCharacters.IsMatch(key))
+                {
+                    Console.WriteLine($"The letter {key} is invalid. Try again.");
+                    continue;
+                }
+
+                if (letters.Contains(key))
+                {
+                    Console.WriteLine("You have already entered this letter!");
+                    continue;
+                }
+
+                letters.Add(key);
+
+                if (!wordToGuess.Contains(key))
+                {
+                    lives--;
+
+                    if (lives > 0)
+                    {
+                        Console.WriteLine("The letter {0} is not in the word to guess. You have {1} {2} left", key, lives, lives == 1 ? "life" : "lives");
+                    }
+                }
+
+                if (lives > 0)
+                {
+                    Console.WriteLine($"You have {lives} lives left!");
+                }
+                else
+                {
+                    Console.WriteLine($"You have lost! The word was {wordToGuess}.");
+                }
             }
+
+
         }
     }
 }
